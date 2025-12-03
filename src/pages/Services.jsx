f7b10634e3e127a5cf1fa35e0b3867510755025a
J E F <jefaraz@gmail.com>
2025-12-03 20:20:08 +0000

    File changes

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -530,6 +530,11 @@ export default function Services() {
       {/* How our services deliver */}
       <section className="py-24 bg-[#faebe3]">
         <div className="max-w-7xl mx-auto px-6">
+          <div className="text-center max-w-4xl mx-auto">
+            <h2 className="text-[2.7rem] md:text-[3.45rem] font-bold text-black mb-12">How our services deliver:</h2>
+            <p className="text-xl text-black/60 font-medium mb-12">Working alone or together to meet your specific needs</p>
+          </div>
+
           <div className="grid lg:grid-cols-2 gap-12 items-center">
              <div className="order-2 lg:order-1">
                 <img 
@@ -539,7 +544,6 @@ export default function Services() {
                 />
              </div>
              <div className="order-1 lg:order-2">
-                <h2 className="text-[2.7rem] md:text-[3.45rem] font-bold text-black mb-12">How our services deliver:</h2>
                 <div className="space-y-8">
                     <div className="mb-6">
                         <p className="text-xl md:text-2xl text-black/60 leading-relaxed">
