bf9fc02bec4f548c7b855b8a362b6836c1195dc6
J E F <jefaraz@gmail.com>
2025-12-14 10:50:45 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -26,6 +26,7 @@ import wellnessCenters from './pages/wellness-centers';
 import wellnessGyms from './pages/wellness-gyms';
 import whatExactlyDoesMusicCuratorDo from './pages/what-exactly-does-music-curator-do';
 import musicForHotelsAndResorts from './pages/music-for-hotels-and-resorts';
+import musicForRestaurantsAndBars from './pages/music-for-restaurants-and-bars';
 import __Layout from './Layout.jsx';
 
 
@@ -58,6 +59,7 @@ export const PAGES = {
     "wellness-gyms": wellnessGyms,
     "what-exactly-does-music-curator-do": whatExactlyDoesMusicCuratorDo,
     "music-for-hotels-and-resorts": musicForHotelsAndResorts,
+    "music-for-restaurants-and-bars": musicForRestaurantsAndBars,
 }
 
 export const pagesConfig = {
