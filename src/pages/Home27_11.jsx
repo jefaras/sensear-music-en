0ec3803deaffe39e2545120f6dfc99b632a62e72
J E F <jefaraz@gmail.com>
2025-12-15 13:30:08 +0000

    Fix Home-test-new.jsx build

--- src/pages/Home27_11.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home2711() {
-  return (
-    <div className="min-h-screen bg-[#faebe3] flex items-center justify-center">
-      <p>Home 27/11 page</p>
-    </div>
-  );
-}
--- src/pages/PlaylistsDemos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function PlaylistsDemos() {
-  return (
-    <div className="min-h-screen bg-[#faebe3] flex items-center justify-center">
-      <p>Playlists & Demos page</p>
-    </div>
-  );
-}
