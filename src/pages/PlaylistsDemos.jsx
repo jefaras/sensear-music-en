67b831a4f49100a6a004f3be594e3e65ce250b78
J E F <jefaraz@gmail.com>
2025-12-15 13:28:46 +0000

    File changes

---
+++ src/pages/PlaylistsDemos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PlaylistsDemos() {
+  return (
+    <div className="min-h-screen bg-[#faebe3] flex items-center justify-center">
+      <p>Playlists & Demos page</p>
+    </div>
+  );
+}
